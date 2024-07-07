54c6e09c620fe38eaacfba9505f5054b88d19489
AsterotthCR <sheancordero97@gmail.com>
2024-07-06 23:33:20 -0500

    Starting Config for the project

---
+++ src/main/java/com/geshk/eldercare/EldercareApplication.java
@@ -0,0 +1,13 @@
+package com.geshk.eldercare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EldercareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EldercareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geshk/eldercare/EldercareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geshk.eldercare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EldercareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
